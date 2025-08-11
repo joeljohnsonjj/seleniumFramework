65ae3fe957460c08a867e3cd35e593e3d4295068
joeljohnsonjj <joelmdnalt@gmail.com>
2025-08-11 11:31:28 +0530

    Added updated drivers, changed paths to content root

--- src/test/java/pages/objectRepository.java
+++ src/test/java/pages/objectRepository.java
@@ -6,5 +6,5 @@ public class objectRepository {
     public static String passwordField = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[2]/input[1]";
     public static String homepageLogo = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]";
     public static String loginPage = "https://www.saucedemo.com/";
-    public static String validTestLoginsFixture = "C:\\Users\\JoelJohnson(G10XIND)\\Desktop\\sampleBddFramework\\src\\test\\java\\fixtures\\validTestLogins.json";
+    public static String validTestLoginsFixture = "src/test/java/fixtures/validTestLogins.json";
 }
